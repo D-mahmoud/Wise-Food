650a69938eb1afe968bca2f769f03df8b0a43bf8
shahd <you@example.com>
2020-12-01 21:05:33 +0200

    Font

--- lib/home.dart
+++ lib/home.dart
@@ -76,35 +76,53 @@ class _HomePageeState extends State<HomePage>
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
+      body: Container(
+        decoration: new BoxDecoration(color: Colors.grey[300]),
+        child: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    child: Text(
+                      'Order food online in Egypt!\n\n Your everyday, right way.           ',
+                      //'Wise-Food  \n\n A service to Trust',
+                      textAlign: TextAlign.center,
+                      //style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                      style: TextStyle(
+                        fontFamily: 'MajorMono',
+                        //fontFamily: 'Pacifico',
+                        //fontFamily: 'Texturina',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                        shadows: [
+                          Shadow(
+                            color: Colors.lightGreen,
+                            blurRadius: 10.0,
+                            offset: Offset(5.0, 5.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                    height: animation.value,
+                    width: animation.value,
+                    child: Image(image: AssetImage('images/logo.png'))),
+                RaisedButton(
+                  color: Colors.green[100],
+                  onPressed: () {},
                   child: Text(
-                    'Order food online in Egypt!                 Your everyday, right way.           ',
+                    'Start Exploring',
                     textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                   ),
                 ),
-              ),
-              Container(
-                  height: animation.value,
-                  width: animation.value,
-                  child: Image(image: AssetImage('images/logo.png'))),
-              RaisedButton(
-                color: Colors.green[100],
-                onPressed: () {},
-                child: Text(
-                  'Start Exploring',
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-              ),
-            ]),
+              ]),
+        ),
       ),
     );
   }
