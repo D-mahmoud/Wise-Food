2b4c422a604833f9c8c97aa3e033d3890a5ee7f2
samiraRefaat <samira1709989@miuegypt.edu.eg>
2021-02-09 18:54:30 +0200

    Update land_page.dart

--- lib/screens/land_page.dart
+++ lib/screens/land_page.dart
@@ -1,1 +1,99 @@
 
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Builder(
+        builder: (context) => Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Image.asset(
+                  "images/logo.png",
+                  alignment: Alignment.topCenter,
+                  fit: BoxFit.contain,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        labelText: "Email",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0))),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter data';
+                      }
+
+                      return null;
+                    },
+                    onSaved: (value) {
+                      print('saved value is $value');
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        labelText: "Password",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0))),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter data';
+                      } else {}
+                      return null;
+                    },
+                    onSaved: (value) {
+                      print('saved value is $value');
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.green[300],
+                    child: MaterialButton(
+                      minWidth: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                    
+                      },
+                    
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
