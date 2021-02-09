bbe67297d917bc59c63583fac740fa91a176dae7
samiraRefaat <samira1709989@miuegypt.edu.eg>
2021-02-09 19:00:17 +0200

    Update land_page.dart

--- lib/screens/land_page.dart
+++ lib/screens/land_page.dart
@@ -1,1 +1,70 @@
 
+
+
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+clasr(32.0))),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter data';
+                      }
+
+                      return null;
+                    },
+                    onSaved: (value) {
+                      print('saved value is $value');
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        contentPadding:
+                            EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        labelText: "Password",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(32.0))),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter data';
+                      } else {}
+                      return null;
+                    },
+                    onSaved: (value) {
+                      print('saved value is $value');
+                    },
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Material(
+                    elevation: 5.0,
+                    borderRadius: BorderRadius.circular(30.0),
+                    color: Colors.green[300],
+                    child: MaterialButton(
+                      minWidth: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                    
+                      },
+                    
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
