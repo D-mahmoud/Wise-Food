1660a217300071fe7bafe1069beb6bb4cbd21fa0
Your Name <samira1709989@miuegypt.edu.eg>
2021-03-08 14:40:48 +0200

    ..

--- lib/widgets/stores/googleMap.dart
+++ lib/widgets/stores/googleMap.dart
@@ -6,18 +6,10 @@ import 'package:wisefood/providers/auth.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Maps extends StatefulWidget {
-  static double longitude;
-  static double latitude;
-  Maps(longitude, latitude);
-  LatLng center = LatLng(longitude, latitude);
-  _MapsState createState() => _MapsState(center);
+  _MapsState createState() => _MapsState();
 }
 
 class _MapsState extends State<Maps> {
-  static LatLng center;
-
-  _MapsState(center);
-
   GoogleMapController mapController;
 
   void _onMapCreated(GoogleMapController controller) {
@@ -26,13 +18,13 @@ class _MapsState extends State<Maps> {
 
   Widget build(BuildContext context) {
     final map = Provider.of<GMap>(context, listen: false);
-
     final authData = Provider.of<Auth>(context, listen: false);
+    final LatLng _center = LatLng(map.latitude, map.longitude);
     return Scaffold(
         body: GoogleMap(
             onMapCreated: _onMapCreated,
             initialCameraPosition: CameraPosition(
-              target: center,
+              target: _center,
               zoom: 11.0,
             )));
   }
