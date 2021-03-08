370c6667396e76c4d5e9cbbbc7ebeee778f9018a
Your Name <samira1709989@miuegypt.edu.eg>
2021-03-08 19:25:17 +0200

    nearby updated

--- lib/widgets/stores/nearBy.dart
+++ lib/widgets/stores/nearBy.dart
@@ -4,15 +4,16 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:wisefood/providers/stores.dart';
 import 'package:geolocator/geolocator.dart';
 
-class Maps extends StatefulWidget {
+class Maps2 extends StatefulWidget {
   @override
-  _MapsState createState() => _MapsState();
+  _Maps2State createState() => _Maps2State();
 }
 
-class _MapsState extends State<Maps> {
+class _Maps2State extends State<Maps2> {
   GoogleMapController mapController;
   var position;
   var loadedMaps;
+  var radius = 2500;
   final Map<String, Marker> _markers = {};
   void _onMapCreated(GoogleMapController controller) {
     loadedMaps = Provider.of<Stores>(
@@ -23,16 +24,20 @@ class _MapsState extends State<Maps> {
     mapController = controller;
     setState(() {
       _markers.clear();
-
-      final marker = Marker(
-        markerId: MarkerId(loadedMaps.storeTitle),
-        position: LatLng(loadedMaps.latitude, loadedMaps.longitude),
-        infoWindow: InfoWindow(
-          title: loadedMaps.storeTitle,
-          snippet: loadedMaps.location,
-        ),
-      );
-      _markers[loadedMaps.storeTitle] = marker;
+      for (final s in loadedMaps) {
+        var p = LatLng(s.latitude, s.longitude);
+        if (position - p <= radius) {
+          final marker = Marker(
+            markerId: MarkerId(s.storeTitle),
+            position: LatLng(s.latitude, s.longitude),
+            infoWindow: InfoWindow(
+              title: s.storeTitle,
+              snippet: s.location,
+            ),
+          );
+          _markers[s.storeTitle] = marker;
+        }
+      }
     });
   }
 
