bdde254f1d353e6b406b93e0ac950f2e7fdd41e8
shahd <shahd1701605@miuegypt.edu.eg>
2021-03-09 15:40:59 +0200

    splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -5,13 +5,15 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: // Text("loading"),
-            Image.asset(
-          'images/logo.jpg',
-          alignment: Alignment.topCenter,
-          fit: BoxFit.contain,
+        child:  Text("loading",style:TextStyle(
+          fontSize: 50
+        ),),
+          //   Image.asset(
+          // 'images/logo.jpg',
+          // alignment: Alignment.topCenter,
+          // fit: BoxFit.contain,
         ),
-      ),
+      //),
     );
   }
 }
